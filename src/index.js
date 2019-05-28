6c581b0621f372c6ff1adc19343eea40c31535a5
infosalim <salimh5757@gmail.com>
2019-05-28 17:03:24 +0600

    finished the header and footer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import './Resources/css/app.css';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './Routes';
+;
+
+const App = () => {
+    return(
+        <BrowserRouter>
+            <Routes/>
+        </BrowserRouter>
+    );
+}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
