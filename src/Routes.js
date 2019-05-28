e02b2bc5928099ad317168baea12189b3f49048d
infosalim <salimh5757@gmail.com>
2019-05-28 19:46:13 +0600

    created home bar animation

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Layout from './Hoc/Layout'
+import { Switch, Route } from 'react-router-dom';
+
+import Home from './Components/Home/Index';
 
 const Routes = (props) => {
   return(
     <div>
       <Layout>
+        <Switch>
+          <Route exact component={Home} path="/" />
+        </Switch>
       </Layout>
     </div>
   );
