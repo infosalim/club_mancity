e572405aa383572cef0f27995ac6eb284d5ebf58
infosalim <salimh5757@gmail.com>
2019-05-30 01:24:56 +0600

    created reusable tag component

--- src/Components/Home/Index.js
+++ src/Components/Home/Index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Featured from './Featured/Index';
+import Matches from './Matches/Index';
 
 const Home = () => {
     return (
         <div className="bck_blue">
             <Featured/>
+            <Matches/>
         </div>
     );
 };
---
+++ src/Components/Home/Matches/Index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Tag } from '../../UI/Misc';
+ 
+const MatchesHome = () => {
+    return (
+        <div className="home_matches_wrapper">
+            <div className="container">
+                <Tag
+                    bck="#0e1731"
+                    size="50px"
+                    color="#fff"
+                >
+                    Matches
+                </Tag>
+
+                BLOCK
+
+                <Tag
+                    bck="#fff"
+                    size="22px"
+                    color="#0e1731"
+                    link={true}
+                    linkTo="/the_team"
+                >
+                    See More Matches
+                </Tag>
+            </div>
+        </div>
+    );
+};
+
+export default MatchesHome;
