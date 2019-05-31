f893a97963492e88d93e0386a0293d08d6a995bb
infosalim <salimh5757@gmail.com>
2019-05-31 22:57:13 +0600

    created the meet player text area

--- src/Components/Home/Index.js
+++ src/Components/Home/Index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Featured from './Featured/Index';
 import Matches from './Matches/Index';
+import MeetPlayers from './MeetPlayers/Index';
 
 const Home = () => {
     return (
         <div className="bck_blue">
             <Featured/>
             <Matches/>
+            <MeetPlayers/>
         </div>
     );
 };
