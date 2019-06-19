0aab3b80082e2fbe35ef4ac26132e0f7c92750d2
infosalim <salimh5757@gmail.com>
2019-06-19 15:50:39 +0600

    created private routes

--- src/Routes.js
+++ src/Routes.js
@@ -2,16 +2,19 @@ import React from 'react';
 import Layout from './Hoc/Layout'
 import { Switch, Route } from 'react-router-dom';
 
+import PrivateRoute from './Components/AuthRoutes/PrivateRoutes';
+
 import Home from './Components/Home/Index';
 import SignIn from './Components/Signin/Index';
 import Dashboard from './Components/Admin/Dashboard';
 
 const Routes = (props) => {
+  console.log(props);
   return(
     <div>
       <Layout>
         <Switch>
-          <Route exact component={Dashboard} path="/dashboard" />
+          <PrivateRoute {...props} exact component={Dashboard} path="/dashboard" />
           <Route exact component={SignIn} path="/sign_in" />
           <Route exact component={Home} path="/" />
         </Switch>
--- src/index.js
+++ src/index.js
@@ -2,21 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './Resources/css/app.css';
 import { BrowserRouter } from 'react-router-dom';
+import { firebase } from './firebase';
 
 import Routes from './Routes';
 
-const App = () => {
+const App = (props) => {
     return(
         <BrowserRouter>
-            <Routes/>
+            <Routes {...props} />
         </BrowserRouter>
     );
 }
 
+firebase.auth().onAuthStateChanged(user => {
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-
+    ReactDOM.render(<App user={user} />, document.getElementById('root'));
+});
