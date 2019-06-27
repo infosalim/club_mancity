90286001d11b28634fc06aadbe80584cbfa33126
infosalim <salimh5757@gmail.com>
2019-06-28 02:34:40 +0600

    finished edit match

--- src/Components/Admin/Matches/AddEditMatch.js
+++ src/Components/Admin/Matches/AddEditMatch.js
@@ -181,17 +181,17 @@ class AddEditMatch extends Component {
         });
     }
 
-    updateFields(match, teamOptions, teams, type, matchId){
+    updateFields(match, teamOptions, teams, type, matchId) {
         const newFormdata = {
             ...this.state.formdata
         }
 
-        for(let key in newFormdata){
-            if(match){
+        for (let key in newFormdata) {
+            if (match) {
                 newFormdata[key].value = match[key];
                 newFormdata[key].valid = true;
             }
-            if(key === 'local' || key==='away'){
+            if (key === 'local' || key === 'away') {
                 newFormdata[key].config.options = teamOptions;
             }
         }
@@ -203,14 +203,14 @@ class AddEditMatch extends Component {
         })
     }
 
-    componentDidMount(){
+    componentDidMount() {
         const matchId = this.props.match.params.id;
         const getTeams = (match, type) => {
-            firebaseTeams.once('value').then(snapshot=>{
+            firebaseTeams.once('value').then(snapshot => {
                 const teams = firebaseLooper(snapshot);
                 const teamOptions = [];
 
-                snapshot.forEach(childSnapshot=>{
+                snapshot.forEach(childSnapshot => {
                     teamOptions.push({
                         key: childSnapshot.val().shortName,
                         value: childSnapshot.val().shortName
@@ -219,17 +219,66 @@ class AddEditMatch extends Component {
                 this.updateFields(match, teamOptions, teams, type, matchId)
             })
         }
-        
-        if(!matchId){
+
+        if (!matchId) {
             // Add Match
-        }else{
+        } else {
             firebaseDB.ref(`matches/${matchId}`).once('value')
-                .then(snapshot=>{
+                .then(snapshot => {
                     const match = snapshot.val();
                     getTeams(match, 'Edit Match')
                 })
         }
     }
+
+    successForm(message) {
+        this.setState({
+            formSuccess: message
+        });
+
+        setTimeout(() => {
+            this.setState({
+                formSuccess: ''
+            })
+        }, 2000);
+    }
+
+    submitForm = (event) => {
+        event.preventDefault();
+
+        let dataToSubmit = {};
+        let formIsValid = true;
+
+        for (let key in this.state.formdata) {
+            dataToSubmit[key] = this.state.formdata[key].value;
+            formIsValid = this.state.formdata[key].valid && formIsValid;
+        }
+
+        this.state.teams.forEach(team => {
+            if (team.shortName === dataToSubmit.local) {
+                dataToSubmit['localThmb'] = team.thmb
+            }
+            if (team.shortName === dataToSubmit.away) {
+                dataToSubmit['awayThmb'] = team.thmb
+            }
+        })
+
+        if (formIsValid) {
+            if (this.state.formType === 'Edit Match') {
+                firebaseDB.ref(`matches/${this.state.matchId}`)
+                    .update(dataToSubmit).then(() => {
+                        this.successForm('Updated Correctly');
+                    }).catch(e => {
+                        this.setState({ formError: true })
+                    })
+            }
+
+        } else {
+            this.setState({
+                formError: true
+            });
+        }
+    };
 
     render() {
         return (
@@ -309,7 +358,7 @@ class AddEditMatch extends Component {
                             </div>
 
                             <div className="success_label">{this.state.formSuccess}</div>
-                            { this.state.formError ?
+                            {this.state.formError ?
                                 <div className="error_label">
                                     Something is wrong
                             </div>
@@ -317,7 +366,7 @@ class AddEditMatch extends Component {
                             }
 
                             <div className="admin_submit">
-                                <button onClick={event=> this.submitForm(event)}>
+                                <button onClick={event => this.submitForm(event)}>
                                     {this.state.formType}
                                 </button>
                             </div>
