113fb6f6863c543d2346dcc14113e1aa19c1eb5e
infosalim <salimh5757@gmail.com>
2019-07-04 02:20:57 +0600

    created file uploader input

--- src/Components/UI/FileUploader.js
+++ src/Components/UI/FileUploader.js
@@ -10,6 +10,34 @@ class Fileuploader extends Component {
         fileURL: ''
     }
 
+    handleUploadStart = () => {
+        this.setState({
+            isUploading: true
+        })
+    }
+    handleUploadError = () => {
+        this.setState({
+            isUploading: false
+        })
+    }
+    handleUploadSuccess = (filename) => {
+        console.log(filename);
+
+        this.setState({
+            name: filename,
+            isUploading: false
+        });
+
+        firebase.storage().ref(this.props.dir).child(filename).getDownloadURL()
+            .then( url => {
+                console.log(url);
+
+                this.setState({
+                    fileURL: url
+                })
+            })
+    }
+
     static getDerivedStateFormProps(props, state) {
         if (props.defaultImg) {
             return state = {
@@ -38,6 +66,31 @@ class Fileuploader extends Component {
                     </div>
 
                     : null
+                }
+                { this.state.isUploading ? 
+                    <div className="progress"
+                        style={{textAlign:'center', margin:'30px 0'}}>
+                        <CircularProgress
+                            style={{color: '#98c6e9'}}
+                            thickness={7}
+                        />
+                    </div>
+                    : null
+                }
+                { this.state.fileURL ?
+                    <div className='image_upload_container'>
+                        <img 
+                            style={{
+                                width: '100%'
+                            }}
+                            src={this.state.fileURL}
+                            alt={this.state.name}
+                        />
+                        <div className="remove" onClick={()=>this.uploadAgain()}>
+                            Remove
+                        </div>
+                    </div>
+                    :null
                 }
             </div>
         );
--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/app';
 import 'firebase/database';
 import 'firebase/auth';
+import 'firebase/storage';
 
 const firebaseConfig = {
     apiKey: "AIzaSyA8sJnl6AzhRYPEUk2sd6I4SUXnONoONW0",
